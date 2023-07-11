e7cbd5e5b193921ae85233181fe9711d7909178b
Minjae <123823158+minseye@users.noreply.github.com>
2023-07-11 13:39:15 +0900

    Update image_captioning_train.py

--- image_captioning_train.py
+++ image_captioning_train.py
@@ -1,6 +1,6 @@
 # 파일명: image_classification_train.py
 
-from platform_image_captioning_train_sub import exec_train, exec_inference
+from platform_image_captioning_train_sub import exec_train, exec_inference, exec_init_svc
 
 import logging
 
@@ -27,19 +27,19 @@ def inference(df, params, batch_id):
     return { **result }
 
 
-if __name__=='__main__':
-    class TM:
-        param_info = {}
-        def __init__(self):
-            self.train_data_path = './meta_data/image'
-            self.model_path = './meta_data/model'
-            self.label_path = './meta_data/annotations'
-            self.output_path = './mata_data/output'
-            # 사용자 param 사용시 입력
-            self.param_info['batch_size'] = 16
-            self.param_info['epoch'] = 30
-            self.param_info['learning_rate'] = 1e-6
-            self.param_info['optimizer'] = 0
+# if __name__=='__main__':
+#     class TM:
+#         param_info = {}
+#         def __init__(self):
+#             self.train_data_path = './meta_data/image'
+#             self.model_path = './meta_data/model'
+#             self.label_path = './meta_data/annotations'
+#             self.output_path = './mata_data/output'
+#             # 사용자 param 사용시 입력
+#             self.param_info['batch_size'] = 16
+#             self.param_info['epoch'] = 30
+#             self.param_info['learning_rate'] = 1e-6
+#             self.param_info['optimizer'] = 0
 
-    tm = TM()
-    exec_train(tm)
+#     tm = TM()
+#     exec_train(tm)
